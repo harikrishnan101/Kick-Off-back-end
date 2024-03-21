79f55c69f76294eebd692f78a2808da73fc97942
HARIKRISHNAN R <harikrishnan183hari@gmail.com>
2024-03-21 22:21:52 +0530

    cors updated

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
-      const conn = await mongoose.connect(`mongodb://localhost:27017/KickOff`, {
+      const conn = await mongoose.connect(`mongodb+srv://harikrishnan183hari:0qNcWKAGCveVJT82@cluster0.psz4owe.mongodb.net/`, {
         // useNewUrlParser: true,
       });
       console.log(`connectDB`);
