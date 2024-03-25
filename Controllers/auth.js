c97e64d6cbfa2299d633565535281faf0efde478
HARIKRISHNAN R <harikrishnan183hari@gmail.com>
2024-03-25 16:01:44 +0530

    hosting api2

--- Controllers/auth.js
+++ Controllers/auth.js
@@ -26,44 +26,30 @@ const signUp=(req,res)=>{
   }}
 
 
-const login = async (req, res) => {
-  try {
-    const user = await USER.findOne({ email: req.body.email });
-
-    if (user) {
-      // Compare the provided password with the hashed password stored in the database
-      bcrypt.compare(req.body.password, user.password, function (err, result) {
-        if (result) {
-          // Passwords match, generate JWT token for authentication
-          const token = jwt.sign({
-            userId: user._id,
-            email: user.email,
-            firstname: user.firstname,
-            lastname: user.lastname,
-            role: user.role
-          }, process.env.JWT_KEY, {
+  const login= async (req,res)=>{
+    try {
+      const user= await USER.findOne({email:req.body.email})
+      
+      
+      if(user){
+       bcrypt.compare(req.body.password, user.password, function(err,resp) {
+        if(resp){
+          const token = jwt.sign({ userId:user._id,email:user.email,firstname:user.firstname,lastname:user.lastname,role:user.role},process.env.JWT_KEY, {
             expiresIn: '2d'
-          });
-
-          // Clear password and ConfirmPassword fields before sending the user object in the response
-          user.password = undefined;
-          user.ConfirmPassword = undefined;
-
-          res.status(200).json({ login: true, token: token, user: user });
+           
+        })
+        user.password = undefined;
+    user.ConfirmPassword = undefined;
+       
+          res.status(200).json({ login: true, token:token,user:user});
         } else {
-          // Passwords do not match, send an error response
           res.status(403).json({ login: false, message: 'Invalid credentials' });
         }
       });
-    } else {
-      // User with the provided email not found, send an error response
-      res.status(404).json({ login: false, message: 'User not found' });
+      }
+    } catch (error) {
+      
     }
-  } catch (error) {
-    // Handle other potential errors
-    console.error('Login failed:', error);
-    res.status(500).json({ login: false, message: 'Internal server error' });
-  }
-}
+    }
 
 module.exports={signUp,login}
