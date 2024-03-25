75f2966fe70a9d2d697951e2f5f1da94ec326648
HARIKRISHNAN R <harikrishnan183hari@gmail.com>
2024-03-25 18:48:55 +0530

    Complete 2

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        // MongoDB connection string
-        const mongoURI = 'mongodb+srv://ramu:njhghv cbasb6766787y8xm VGFC@cluster0.psz4owe.mongodb.net/';
+      
+        const mongoURI = 'mongodb+srv://harikrishnan183hari:0qNcWKAGCveVJT82@cluster0.psz4owe.mongodb.net/';
 
-        // Connect to MongoDB
+        
         await mongoose.connect(mongoURI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
@@ -16,7 +16,7 @@ const connectDB = async () => {
         console.log('MongoDB connected successfully');
     } catch (error) {
         console.error('MongoDB connection error:', error.message);
-        // Terminate the process if unable to connect to the database
+        
         process.exit(1);
     }
 };
