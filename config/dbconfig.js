14f65acb5b41cc696a7a2865ef98d6403833316d
HARIKRISHNAN R <harikrishnan183hari@gmail.com>
2024-03-26 12:14:55 +0530

    final commit4

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
+      const conn = await mongoose.connect(`mongodb+srv://harikrishnan183hari:0qNcWKAGCveVJT82@cluster0.zmo7ovh.mongodb.net/`);
       
-      const conn = await mongoose.connect(`mongodb+srv://harikrishnan183hari:cybdNs6qwbWEL04e@cluster0.zmo7ovh.mongodb.net/`);
       // const conn = await mongoose.connect(`mongodb://localhost:27017/KickOff`);
       
       console.log(`connectDatabase`);
-    } catch (error) {
-      console.error(error.message);
+    } catch (err) {
+      console.error(err.message);
       
     }
   }
